7ccae1292b50be45e72b9da58ec8790ea0e46239
eutro <benedek.szilvasy@gmail.com>
2022-02-25 21:24:10 +0000

    Exclude examples from build

--- info.rkt
+++ info.rkt
@@ -1,8 +1,9 @@
 #lang info
 (define collection "ecktra")
-(define deps '("base" "draw-lib" "gui-lib" "math-lib" "pict-lib" "typed-racket-lib" "typed-racket-more"))
+(define deps '("base" "draw-lib" "gui-lib" "math-lib" "pict-lib" "typed-racket-lib" "typed-racket-more" "rsvg"))
 (define build-deps '("scribble-lib" "rackunit-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/ecktra.scrbl" ())))
+(define compile-omit-paths '("examples"))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(eutro))
