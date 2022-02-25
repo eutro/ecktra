921c9e37054fc8e8f1b77256cd346b3afe5b2fc1
eutro <benedek.szilvasy@gmail.com>
2022-02-25 22:46:45 +0000

    Add threading dep >:(

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
 (define collection "ecktra")
-(define deps '("base" "draw-lib" "gui-lib" "math-lib" "pict-lib" "typed-racket-lib" "typed-racket-more" "rsvg"))
+(define deps '("base" "draw-lib" "gui-lib" "math-lib" "pict-lib" "typed-racket-lib" "typed-racket-more" "rsvg" "threading"))
 (define build-deps '("scribble-lib" "rackunit-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/ecktra.scrbl" ())))
-(define compile-omit-paths '("examples"))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(eutro))
